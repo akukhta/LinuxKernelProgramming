74efb1af084993d7afe84d0ce8bfc07b72437db2
akukhta <electricsix23@gmail.com>
2021-09-21 16:26:51 +0300

    Some fixes for task 5

--- Task5/solution.c
+++ Task5/solution.c
@@ -50,15 +50,10 @@ static int __init deviceInit(void)
 	myDevice = (struct _device *) kmalloc(sizeof(struct _device), GFP_KERNEL);
 	devID = MKDEV(major, minor);
 
-	printk(KERN_INFO "Major %i, minor %i\n", MAJOR(devID), MINOR(devID));
-
 	if (register_chrdev_region(devID, 1, name) != 0)
 	{
-		printk(KERN_ERR "Cannot register char device region\n");
-		return 1;
+		return -1;
 	}
-	
-	printk(KERN_INFO "HERE\n");
 
 	cdev_init(&myDevice->c_dev, &devOPS);
 	myDevice->c_dev.owner = THIS_MODULE;
@@ -67,22 +62,17 @@ static int __init deviceInit(void)
 	myDevice->totalDataWritten = 0;
 	myDevice->deviceNumber = MKDEV(major, minor + 1);
 
-	printk(KERN_INFO "After initing\n");
-
 	if (cdev_add(&myDevice->c_dev, myDevice->deviceNumber, 1))
 	{
-		printk(KERN_ERR "Failed to Add device number\n");
-		return 2;
+		return -2;
 	}	
-	
-	printk(KERN_INFO "Before exit\n");	
+		
 	return 0;
 }
 
 static void __exit deviceExit(void)
 {
 	cdev_del(&myDevice->c_dev);
-	printk(KERN_INFO "Device's major: %i, minor: %i\n", MAJOR(myDevice->deviceNumber), MINOR(myDevice->deviceNumber)); 
 	unregister_chrdev_region(MKDEV(major, minor), 1);
 	return;
 }
